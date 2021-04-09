0b28b5cfb8091c2e29d0b3ac9a48063ebb7ad7b9
StevenPss <pulessteven@gmail.com>
2021-04-09 14:55:04 +0200

    style: added simple gsap animations on contents

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import './App.scss';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import { TweenMax } from 'gsap';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  //variable declaration
+  let app = useRef(null);
+
+  // when dom loads
+  useEffect(() => {
+    //App
+    TweenMax.to(
+      app.current,
+      {
+        duration: .5,
+        css: {
+          visibility: 'visible'
+        }
+      }
+    )
+  }, [])
+
   return (
-    <div className="app w-screen h-screen">
+    <div ref={app} className="app w-screen h-screen">
       <div id="bg" className="h-full w-full absolute"></div>
       <Header/>
       <Main/>
